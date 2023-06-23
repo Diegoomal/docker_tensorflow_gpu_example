e1697287553abe3dd9d5949072b42e389c2da62c
Diego Vasque Maldonado <diegoomal13@gmail.com>
2023-06-23 10:17:29 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Python simple project
+
+Simple example to implement some project in python
+
+(conda virtual environment is required)
+
+## Build and run the project
+
+```
+source pipeline/build.sh
+```
+
+## Remove dir's, files and destroy the virtual environment
+
+```
+source pipeline/destroy.sh
+```
+
+## Update and run the project
+
+```
+source pipeline/update.sh
+```
+
+Links
+
+[tensorflow](https://www.tensorflow.org/install/docker?hl=pt-br)
+
+[stackoverflow](https://stackoverflow.com/questions/59265920/this-tensorflow-binary-is-optimized-with-intelr-mkl-dnn-to-use-the-following-c)
---
+++ pipeline/update.sh
@@ -0,0 +1,34 @@
+
+echo "===== bash update ====="
+
+echo "1) Reset environment"
+
+# source pipeline/destroy.sh
+
+echo "2) Conda environment"
+
+# conda deactivate
+
+conda env update -n project-env -f ./env.yml
+
+conda activate project-env
+
+echo "3) LINT verify with Flak8"
+
+# flake8 . --count --statistics
+
+# flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
+
+echo "4) Generating documentation"
+
+# pdoc src -o docs
+
+echo "5) Run project"
+
+# python src/main.py
+
+docker run --gpus all -it --rm tensorflow/tensorflow:latest-gpu & python src/main.py
+
+echo "6) Unity test with pytest"
+
+# pytest -s
